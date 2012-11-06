0514612a46c9239d86fcff3654bb475d50171593
vgulaev <vgulaev@yandex.ru>
2012-11-06 18:00:27 +0600

    Filed First and Second Drivers

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,55 +29,202 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxAddName = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBoxName1 = new System.Windows.Forms.TextBox();
+            this.textBoxAddName1 = new System.Windows.Forms.TextBox();
+            this.textBoxSurname1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(190, 182);
+            this.button1.Location = new System.Drawing.Point(216, 178);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(129, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Создать документ";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // textBoxSurname
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 13);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(259, 20);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "Гуляев";
+            this.textBoxSurname.Location = new System.Drawing.Point(63, 19);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(197, 20);
+            this.textBoxSurname.TabIndex = 1;
+            this.textBoxSurname.Text = "Иванов";
             // 
-            // textBox2
+            // label1
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 39);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(260, 20);
-            this.textBox2.TabIndex = 2;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Фамилия";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Имя";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Отчество";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(63, 48);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(197, 20);
+            this.textBoxName.TabIndex = 6;
+            this.textBoxName.Text = "Сильвестр";
+            // 
+            // textBoxAddName
+            // 
+            this.textBoxAddName.Location = new System.Drawing.Point(63, 78);
+            this.textBoxAddName.Name = "textBoxAddName";
+            this.textBoxAddName.Size = new System.Drawing.Size(197, 20);
+            this.textBoxAddName.TabIndex = 7;
+            this.textBoxAddName.Text = "Григорьевич";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxName);
+            this.groupBox1.Controls.Add(this.textBoxAddName);
+            this.groupBox1.Controls.Add(this.textBoxSurname);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(270, 160);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Водитель №1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBoxName1);
+            this.groupBox2.Controls.Add(this.textBoxAddName1);
+            this.groupBox2.Controls.Add(this.textBoxSurname1);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(288, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(270, 160);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Водитель №2";
+            // 
+            // textBoxName1
+            // 
+            this.textBoxName1.Location = new System.Drawing.Point(63, 48);
+            this.textBoxName1.Name = "textBoxName1";
+            this.textBoxName1.Size = new System.Drawing.Size(197, 20);
+            this.textBoxName1.TabIndex = 6;
+            this.textBoxName1.Text = "Арнольд";
+            // 
+            // textBoxAddName1
+            // 
+            this.textBoxAddName1.Location = new System.Drawing.Point(63, 78);
+            this.textBoxAddName1.Name = "textBoxAddName1";
+            this.textBoxAddName1.Size = new System.Drawing.Size(197, 20);
+            this.textBoxAddName1.TabIndex = 7;
+            this.textBoxAddName1.Text = "Кирсанович";
+            // 
+            // textBoxSurname1
+            // 
+            this.textBoxSurname1.Location = new System.Drawing.Point(63, 19);
+            this.textBoxSurname1.Name = "textBoxSurname1";
+            this.textBoxSurname1.Size = new System.Drawing.Size(197, 20);
+            this.textBoxSurname1.TabIndex = 1;
+            this.textBoxSurname1.Text = "Петров";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Фамилия";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Отчество";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1, 52);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Имя";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(566, 205);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Заполнение формы справки ДТП";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxAddName;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBoxName1;
+        private System.Windows.Forms.TextBox textBoxAddName1;
+        private System.Windows.Forms.TextBox textBoxSurname1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -26,15 +26,6 @@ namespace FillingRTFDoc
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            Char letterChar = 'Г';
-            Encoding.Unicode.GetBytes("Г");
-            Char.ToString('Г');
-            //textBox2.Text = Char.ConvertToUtf32("U");
-            //textBox2.Text = BitConverter.ToString(Encoding.Unicode.GetBytes("{"));
-            //\'c5
-            textBox2.Text = chartoRTFstyle("Г");
-            //Encoding.GetEncoding("Windows-1251").GetBytes
-            //Encoding.Unicode.GetString(Encoding.Unicode.GetBytes("Г"));
             
             Stream resourseStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("FillingRTFDoc.template_doc.rtf");
             StreamReader tr = new StreamReader(resourseStream);
@@ -42,15 +33,51 @@ namespace FillingRTFDoc
 
             String stringDoc = tr.ReadToEnd();
 
-            //"\'" + BitConverter.ToString(Encoding.GetEncoding("Windows-1251").GetBytes("Г"))
-            stringDoc = stringDoc.Replace("$n01", chartoRTFstyle("Г"));
-            stringDoc = stringDoc.Replace("$n02", chartoRTFstyle("у"));
-            stringDoc = stringDoc.Replace("$n03", chartoRTFstyle("л"));
+            String FIO;
+
+            FIO = textBoxSurname.Text.Trim() + " " + textBoxName.Text.Trim();
+            FIO = FIO.Trim() + " " + textBoxAddName.Text.Trim();
+            FIO.Trim();
+            
+            int indexN = 1;
+            //filing Surname in Doc
+            foreach (char _c in FIO)
+            {
+                stringDoc = stringDoc.Replace("$n" + String.Format("{0:000}",indexN), chartoRTFstyle(_c.ToString()));
+                indexN = indexN + 1;
+            }
+
+            for (int i = indexN; i < 38 ; i++)
+            {
+                stringDoc = stringDoc.Replace("$n" + String.Format("{0:000}", i), "");
+            }
+
+            FIO = textBoxSurname1.Text.Trim() + " " + textBoxName1.Text.Trim();
+            FIO = FIO.Trim() + " " + textBoxAddName1.Text.Trim();
+            FIO.Trim();
+
+            indexN = 101;
+            //filing Surname in Doc
+            foreach (char _c in FIO)
+            {
+                stringDoc = stringDoc.Replace("$n" + String.Format("{0:000}", indexN), chartoRTFstyle(_c.ToString()));
+                indexN = indexN + 1;
+            }
+
+            for (int i = indexN; i < 138; i++)
+            {
+                stringDoc = stringDoc.Replace("$n" + String.Format("{0:000}", i), "");
+            }
 
             tw.Write(stringDoc);
             tw.Close();
 
             Process.Start("data.rtf");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
