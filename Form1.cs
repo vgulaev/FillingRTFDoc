6fb12264fcb6127ad053af61e5e149dcb80eff2c
vgulaev <vgulaev@yandex.ru>
2012-11-06 17:05:43 +0600

    pre Beta version

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
 
 namespace FillingRTFDoc
 {
@@ -15,6 +18,39 @@ namespace FillingRTFDoc
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public string chartoRTFstyle(string str)
+        {
+            return ("\\'" + BitConverter.ToString(Encoding.GetEncoding("Windows-1251").GetBytes(str)));
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Char letterChar = 'Г';
+            Encoding.Unicode.GetBytes("Г");
+            Char.ToString('Г');
+            //textBox2.Text = Char.ConvertToUtf32("U");
+            //textBox2.Text = BitConverter.ToString(Encoding.Unicode.GetBytes("{"));
+            //\'c5
+            textBox2.Text = chartoRTFstyle("Г");
+            //Encoding.GetEncoding("Windows-1251").GetBytes
+            //Encoding.Unicode.GetString(Encoding.Unicode.GetBytes("Г"));
+            
+            Stream resourseStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("FillingRTFDoc.template_doc.rtf");
+            StreamReader tr = new StreamReader(resourseStream);
+            StreamWriter tw = new StreamWriter("data.rtf");
+
+            String stringDoc = tr.ReadToEnd();
+
+            //"\'" + BitConverter.ToString(Encoding.GetEncoding("Windows-1251").GetBytes("Г"))
+            stringDoc = stringDoc.Replace("$n01", chartoRTFstyle("Г"));
+            stringDoc = stringDoc.Replace("$n02", chartoRTFstyle("у"));
+            stringDoc = stringDoc.Replace("$n03", chartoRTFstyle("л"));
+
+            tw.Write(stringDoc);
+            tw.Close();
+
+            Process.Start("data.rtf");
         }
     }
 }
